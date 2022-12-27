88d45c8f01ffc2f1f2c396ea2ce74f9e0a291498
jsathish456 <121101700+jsathish456@users.noreply.github.com>
2022-12-27 15:29:50 +0530

    Add files via upload

---
+++ container_spaces_llc.js
@@ -0,0 +1,70 @@
+ContactDetails = [{
+    "id": 1,
+    "department": "CIVIL",
+    "data": [{
+        "id": 1,
+        "name": "Test",
+        "designation": "MANAGER",
+        "contact_no": "9999888881"
+    },{
+        "id": 2,
+        "name": "Test2",
+        "designation": "SUPERVISOR",
+        "contact_no": "9999888881-1"
+    }]
+},
+{
+    "id": 2,
+    "department": "LANDSCAPE",
+    "data": [{
+        "id": 1,
+        "name": "Test",
+        "designation": "MANAGER",
+        "contact_no": "9999888882"
+    },{
+        "id": 2,
+        "name": "Test2",
+        "designation": "ASSISTANT MANAGER",
+        "contact_no": "9999888881-1"
+    }]
+},
+{
+    "id": 3,
+    "department": "AIR_CONDITION",
+    "data": [{
+        "id": 1,
+        "name": "Test",
+        "designation": "MANAGER",
+        "contact_no": "9999888883"
+    }]
+},
+{
+    "id": 4,
+    "department": "WOOD",
+    "data": [{
+        "id": 1,
+        "name": "Test",
+        "designation": "MANAGER",
+        "contact_no": "9999888884"
+    }]
+},
+{
+    "id": 5,
+    "department": "MEP",
+    "data": [{
+        "id": 1,
+        "name": "Test",
+        "designation": "MANAGER",
+        "contact_no": "9999888885"
+    }]
+},
+{
+    "id": 6,
+    "department": "MISCELLANEOUS",
+    "data": [{
+        "id": 1,
+        "name": "Test",
+        "designation": "MANAGER",
+        "contact_no": "9999888886"
+    }]
+}]
